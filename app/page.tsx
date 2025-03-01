ba45994e56759e2971efa95c3572524fc98c2866
FranciscoYuster <yusterfrancisco@gmail.com>
2025-03-01 16:24:51 -0300

    fusion conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts/fonts';
-import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -27,14 +26,14 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image 
+          <img 
           src="/hero-desktop.png"
           width={1000}
           height={760}
           className='hidden md:block' 
           alt="Screenshots of the dashboard project shwing desktop version" />
         
-          <Image 
+        <img 
           src="/hero-mobile.png"
           width={1000}
           height={760}
